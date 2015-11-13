111ab10fae8e5c8a6e032b42504bafa9bde6c3f1
fabada <dezmo1992@hotmail.com>
2015-11-13 14:31:34 -0800

    no more hard coded requests in the template

--- kitchen_app/web_ui/views.py
+++ kitchen_app/web_ui/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from kitchen_app.models.item import Item
+from kitchen_app.models.request import Request
+
 
 def index(request):
     context = {
         'floor_options': range(2,15),
-        'items': [(item.id, item.name) for item in Item.objects.all()]
+        'items': [(item.id, item.name) for item in Item.objects.all()],
+        'active_requests': [(r.id, r.item.item.name, r.requester.full_name, r.employee_location) for r in Request.objects.filter(status=0)],
     }
     return render(request, 'index.html', context)
