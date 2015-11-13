0ca8b4be3ad387b665a22d82f05b99a8a2195c8f
Daniel Pino <daniel@140-sfoengwifi54-215.clients.corp.yelpcorp.com>
2015-11-13 11:46:57 -0800

    Added web front-end

---
+++ web/js/scripts.js
@@ -0,0 +1,22 @@
+var feedMe = function () {
+    "use strict";
+    var $ = this.$, document = this.document, console = this.console;
+    $(document).ready(function () {
+        console.log("test");
+
+        $('.header-extension').hide();
+        
+        $('.new-order').click(function () {
+            $('.header-extension').slideToggle(300);
+        });
+        
+        $(".grid-toggle").click(function () {
+            $('.order-container').toggleClass('grid');
+            $('.header').slideToggle(300);
+            $('.new-order').slideToggle(300);
+            $('.floor').toggleClass('kitchen-view');
+        });
+    });
+};
+
+feedMe.call(this);
