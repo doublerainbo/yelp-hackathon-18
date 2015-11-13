d7c3725c351aee4af6368052648ef00d67f0105a
Bo Zhao <bozhao@yelp.com>
2015-11-12 16:10:29 -0800

    temp

--- kitchen_app/api/views.py
+++ kitchen_app/api/views.py
@@ -1,4 +1,16 @@
+from django.core import serializers
 from django.http import HttpResponse
-
+from 
 def index(request):
     return HttpResponse("Main API page")
+
+
+def create_request(request):
+	pass
+
+
+def cancel_request(request):
+	pass
+
+
+def ack_request(request):
