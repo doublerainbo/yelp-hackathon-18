4580e30cd93e90d0d7d48b91cd24b17e1ba9d376
Daniel Pino <daniel@140-sfoengwifi54-215.clients.corp.yelpcorp.com>
2015-11-13 12:48:19 -0800

    fixed form close on kitchen view

--- kitchen_app/static/js/scripts.js
+++ kitchen_app/static/js/scripts.js
@@ -10,11 +10,19 @@ var feedMe = function () {
             $('.header-extension').slideToggle(300);
         });
         
+        $('.order-submit').click(function () {
+            $('.header-extension').slideToggle(300);
+        });
+        
         $(".grid-toggle").click(function () {
             $('.order-container').toggleClass('grid');
             $('.header').slideToggle(300);
             $('.new-order').slideToggle(300);
             $('.floor').toggleClass('kitchen-view');
+            
+            if ($('.header-extension').is(':visible')) {
+                $('.header-extension').slideToggle(300);
+            }
         });
     });
 };
