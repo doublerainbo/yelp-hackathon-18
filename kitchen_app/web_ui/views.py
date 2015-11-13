c966e273ce5dc0f702a999e2ad2a38c7f19ab965
fabada <dezmo1992@hotmail.com>
2015-11-13 12:32:29 -0800

    got most of the template rendering

--- kitchen_app/web_ui/views.py
+++ kitchen_app/web_ui/views.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Main web UI page")
+    context = {
+        'floor_options': range(2,15),
+    }
+    return render(request, 'index.html', context)
