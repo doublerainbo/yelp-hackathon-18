609033aeaf86761141a53fd973e62f356d2af183
Daniel Pino <daniel@140-sfoengwifi54-215.clients.corp.yelpcorp.com>
2015-11-13 15:39:35 -0800

    Added api calls to filter floors

--- kitchen_app/static/js/scripts.js
+++ kitchen_app/static/js/scripts.js
@@ -7,11 +7,21 @@ var feedMe = function () {
         $('.header-extension').hide();
         
         $('.new-order').click(function () {
+            $('.new-order').toggleClass('rotate');
             $('.header-extension').slideToggle(300);
         });
         
         $('.order-submit').click(function () {
             $('.header-extension').slideToggle(300);
+        });
+        
+        $(".choose-floor-button").click(function () {
+            $('.choose-floor').slideToggle(300);
+            if ($(".choose-floor-button").text() === "Choose floor") {
+                $(".choose-floor-button").text("Close");
+            } else {
+                $(".choose-floor-button").text("Choose floor");
+            }
         });
         
         $(".grid-toggle").click(function () {
@@ -48,6 +58,35 @@ var feedMe = function () {
                 });
                   
         });
+        
+        $(".requests-floor").change(function () {
+            var floor = $(this).find("option:selected").text();
+            
+            console.log(floor);
+            
+            self.post("http://127.0.0.1:8000/api/kitchen_requests", { floor: floor })
+                .done(function (data) {
+                    console.log("Data Loaded: " + data);
+                
+                    var jsonData = JSON.parse(data);
+                    
+                    self('.all-orders').html('');
+
+                    self.each(jsonData, function (i, item) {
+                        
+                        $('.all-orders').append('<div class="order-container"><div class="order-details"><div class="what">' +
+                                                item.item + '</div><div class="who">'
+                                                + item.requester + ' - located at: '
+                                                + item.employee_location + '</div></div>'
+                                                + '<img class="complete-icon" src="static/images/check.png"></div>');
+                        
+                    });
+                    
+                    
+                });
+                  
+        });
+        
     });
 };
 
