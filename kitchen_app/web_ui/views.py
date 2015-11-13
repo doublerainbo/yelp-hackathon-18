1925d7890705db5fb37f1868729b01d238d49d6a
fabada <dezmo1992@hotmail.com>
2015-11-13 13:35:37 -0800

    picker for snacks

--- kitchen_app/web_ui/views.py
+++ kitchen_app/web_ui/views.py
@@ -2,8 +2,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from kitchen_app.models.item import Item
+
 def index(request):
     context = {
         'floor_options': range(2,15),
+        'items': [item.name for item in Item.objects.all()]
     }
     return render(request, 'index.html', context)
